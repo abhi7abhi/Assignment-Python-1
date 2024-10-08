4d8e6131bfa30a3cf3b342476e675162cce19b45
abhi7abhi <134796990+abhi7abhi@users.noreply.github.com>
2024-10-08 23:40:51 +0530

    Update Github-6.py

--- Github-6.py
+++ Github-6.py
@@ -1,7 +1,9 @@
-#write a program to determine the female population at the end of each year in the last decade. 
-# initial population is 1,00,000.  The population increases by 5% per year in the last decade.
+#Generate a multiplication table for any give number by user.
+#Hint: 
+#Use input()
+#For example: if user input is 5 means, generate the 5th multiplication table for the range 1 to 10.
 
-population=100000
-for year in range(1,11):   #stops at 10
-    population+=(population*0.05)
-    print(f"Year {year}: {int(population)} women employees")
+
+a=int(input("Enter a number to generate a table for: "))
+for i in range(1,11):       #goes till 10 only
+    print(f"{a}*{i}={a*i}")  #prints the table for the given number
