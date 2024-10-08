87a2d83fd76be5118cf1711f8418156e31554c54
abhi7abhi <134796990+abhi7abhi@users.noreply.github.com>
2024-10-08 23:59:07 +0530

    Update Github-6.py

--- Github-6.py
+++ Github-6.py
@@ -5,5 +5,5 @@
 
 
 a=int(input("Enter a number to generate a table for: "))
-for i in range(1,11):       #goes till 10 only
-    print(f"{a}*{i}={a*i}")  #prints the table for the given number
+for i in range(1,11):  
+    print(f"{a}*{i}={a*i}") 
